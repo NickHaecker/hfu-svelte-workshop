c27dbc7525843fd1c2d5de38488410057febee8d
Özkan Yanikbas <oezkan.yanikbas@re-lounge.com>
2022-10-13 13:24:20 +0200

    Improve virtual keybaord animation

--- src/components/keyboard/Key.svelte
+++ src/components/keyboard/Key.svelte
@@ -1,7 +1,7 @@
 <script></script>
 
 <button
-  class="h-14 min-w-[2.75rem] rounded-lg border border-red-500 px-4 font-medium uppercase transition-colors duration-100 hover:bg-red-50 active:bg-red-500 active:text-white"
+  class="h-14 min-w-[2.75rem] rounded-lg border border-red-500 px-4 font-medium uppercase transition-all duration-100 hover:scale-95 hover:bg-red-50 active:scale-90 active:bg-red-500 active:text-white"
   on:click
 >
   <slot />
