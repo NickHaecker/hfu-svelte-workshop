ac52e53e160ecd8e818052a8bb11589168dc2a58
Özkan Yanikbas <oezkan.yanikbas@re-lounge.com>
2022-10-13 09:57:56 +0200

    Refactoring

---
+++ src/components/base/BaseAlert.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let red = false;
+  export let green = false;
+</script>
+
+<div
+  class="mx-auto inline-flex rounded-lg border p-2 px-6
+  {green ? 'border-green-500 bg-green-50' : ''} {red
+    ? 'border-red-500 bg-red-50'
+    : ''}"
+>
+  <slot />
+</div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,26 @@
+import { WORDS } from '../constants';
+
 /**
  * Returns a random integer between min (inclusive) and max (inclusive).
  *
  * @param {Number} min
  * @param {Number} max
  */
-export function getRandomInt(min, max) {
+function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function getRandomWord() {
+  return WORDS[getRandomInt(0, WORDS.length)].toUpperCase();
+}
+
+/**
+ * @param {String} currentWord
+ */
+export function wordExists(currentWord) {
+  return WORDS.filter(
+    (word) => word.toUpperCase() === currentWord.toUpperCase()
+  ).length;
 }
